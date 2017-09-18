ac6b38fbf5299e7b38d6a24ac4a97de009c347b6
Saalihou Ndiaye <saalihou@gmail.com>
2017-09-18 20:27:24 +0000

    feat: Add tab navigation and infos form in TakeMeasures

--- js/screens/TakeMeasures.js
+++ js/screens/TakeMeasures.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { NavigationStackScreenOptions } from 'react-navigation';
+import ViewPager from 'react-native-tabbed-view-pager-android';
 
 import screen from '../hoc/screen';
 import Mannequin from '../components/measure/Mannequin';
+import FormSection from '../components/FormSection';
+import Input from '../components/Input';
+import colors from '../theme/colors.json';
 
 class TakeMeasures extends Component {
   static navigationOptions: NavigationStackScreenOptions = {
@@ -13,7 +17,30 @@ class TakeMeasures extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Mannequin />
+        <ViewPager
+          tabNames={['Mesures', 'Informations']}
+          style={{ flex: 1 }}
+          tabMode="fixed"
+          tabIndicatorColor={colors.ACCENT}
+        >
+          <View style={styles.page}>
+            <Mannequin />
+          </View>
+          <View style={styles.page}>
+            <FormSection submitLabel="Enregistrer">
+              <Input name="client" placeholder="Client" icon="person" />
+              <Input name="phone" placeholder="Téléphone" icon="phone" keyboardType="numeric" />
+              <Input
+                name="notes"
+                placeholder="Notes"
+                multiline
+                numberOfLines={3}
+                autoCapitalize="sentences"
+                style={{ fontSize: 16 }}
+              />
+            </FormSection>
+          </View>
+        </ViewPager>
       </View>
     );
   }
@@ -21,6 +48,9 @@ class TakeMeasures extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  page: {
     flex: 1,
   },
 });
